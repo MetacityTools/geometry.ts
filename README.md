7ebe60d2ff21a518def7dceb5cd3eb92fd48a46a
Vojta Tomas <hello@vojtatom.cz>
2023-06-08 14:17:46 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# geometry.ts 📄
+
+Tiny utility for manipulating geometry in 2D and 3D space.
+
+```
+npm i @vojtatom/geometry
+```
---
+++ vite.config.ts
@@ -0,0 +1,21 @@
+import { defineConfig } from "vite";
+import { resolve } from "path";
+import dts from "vite-plugin-dts";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  build: {
+    lib: {
+      entry: resolve(__dirname, "geometry/geometry.ts"),
+      name: "geometry",
+      // the proper extensions will be added
+      fileName: "geometry",
+    },
+  },
+  resolve: {
+    alias: {
+      "@geometry": resolve(__dirname, "./geometry"),
+    },
+  },
+  plugins: [dts()],
+});
